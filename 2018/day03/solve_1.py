b1d6bc48cf82ef3b9076430f788eef3208654771
Jay Shah <jrshah01@gmail.com>
2020-07-05 11:15:59 +0100

    wip

--- 2018/day03/solve_1.py
+++ 2018/day03/solve_1.py
@@ -1,23 +1,26 @@
 import re
+from collections import namedtuple
 
+
+fabric = namedtuple('fabric', ['id', 'x', 'y', 'width', 'length'])
 
 def find_overlap(input_text):
-    fabrics = re.findall('#\d+ @ (\d+),(\d+): (\d+)x(\d+)', input_text)
+    fabrics = re.findall('#(\d+) @ (\d+),(\d+): (\d+)x(\d+)', input_text)
     covered_points = set()
     overlap_points = set()
-    for fabric in fabrics:
-        fabric = [int(p) for p in fabric]
-        for point in generate_points(fabric):
+    for f in fabrics:
+        f = fabric(*(int(p) for p in f))
+        for point in generate_points(f):
             if point not in covered_points:
                 covered_points.add(point)
             else:
                 overlap_points.add(point)
-    return len(overlap_points)
+    return overlap_points
 
 
 def generate_points(fabric):
-    for x in range(fabric[0], fabric[2] + fabric[0]):
-        for y in range(fabric[1], fabric[3] + fabric[1]):
+    for x in range(fabric.x, fabric.x + fabric.width):
+        for y in range(fabric.y, fabric.y + fabric.length):
             yield x, y
 
 
@@ -28,6 +31,7 @@ def test_find_overlap():
         #3 @ 5,5: 2x2
         """
     assert 4 == find_overlap(input_text)
+
 
 if __name__ == '__main__':
     with open('input_1.txt', 'r') as f:
